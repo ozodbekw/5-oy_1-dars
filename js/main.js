1a097d5fb83898f801294ca6b0a1bcb6e13dd2e1
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-01-08 14:25:42 +0500

    Add additional JavaScript exercises and comments for practice

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 2-amaliyot
 // #1 Topshiriq
 // const ism = prompt("Ismingizni kiriting:");
 // const yosh = +prompt("Yoshingizni kiriting:");
@@ -87,3 +88,183 @@
 // for (let i = 1; i <= 10; i++) {
 //   console.log(`${n}x${i}=${n * i}`);
 // }
+
+// 3-amaliyot
+// #1
+// const myArray = [[false, true], [1, 2], 99, "yaxshi"];
+
+// for (let i = 0; i < myArray.length; i++) {
+//   if (typeof myArray[i] == "string") {
+//     console.log("String index: ", i);
+//   }
+// }
+
+// myArray.forEach((item, i) => {
+//   if (typeof item == "string") {
+//     console.log(i);
+//   }
+// });
+
+// #2
+// const data = [[1, 2, 3], "qizil", [4, 5, 6], true, false];
+// const newData = [];
+
+// data.forEach((item) => {
+//   const respins = Array.isArray(item);
+//   if (respins) {
+//     item.forEach((num) => {
+//       newData.push(num);
+//     });
+//   }
+// });
+// console.log(newData);
+
+// #3
+// const chekData = [null, "false", 0, 99, undefined, false, "", NaN, "0"];
+// const reslult = [];
+// chekData.forEach((item) => {
+//   if (!item) {
+//     reslult.push(item);
+//   }
+// });
+// console.log(reslult);
+
+// 4#
+// const word = "samarqand";
+// const splitWord = word.split("").reverse();
+// let newWord = "";
+
+// for (let i = 0; i < splitWord.length; i++) {
+//   newWord += splitWord[i];
+// }
+// console.log(newWord)
+
+// #5
+// const word = "AssAlomU AlaYkuM";
+// const splitWord = word.split(" ");
+// let newWord = "";
+
+// splitWord.forEach((item) => {
+//   const bosh = item.charAt().toLowerCase();
+//   const davomi = item.slice(1).toUpperCase();
+//   newWord += " " + (bosh + davomi);
+// });
+
+// console.log(newWord)
+
+// #6
+// const numbers = [-1, 87, -5.5, Infinity, 0, 1, 5, 3, 0.9];
+// let newNum = 0;
+// numbers.forEach((num) => {
+//   if (num >= 0.0 && Math.floor(num) === num && num !== Infinity && num != 0) {
+//     newNum += num;
+//   }
+// });
+// console.log(newNum);
+
+// #7
+
+// const word = "AssAlomU AlaYkuM";
+// let splitWord = word.split("");
+// let counter = 0;
+// splitWord.forEach((word) => {
+//   if (word == "a" || word == "A") {
+//     counter++;
+//   }
+// });
+// console.log(counter)
+
+// #8
+
+// const types = [21, "Robiya", "array", ["I'm array"], null, true, 215];
+// let nweTypes = [];
+
+// types.forEach((type) => {
+//   nweTypes.push(typeof type);
+// });
+// console.log(nweTypes);
+
+// #9
+
+// const word = "Uzbekistan";
+// console.log(word.lastIndexOf(word.slice(-1)) + 1);
+
+// #10
+
+// const numbers = [1, 2, 3, 1, 5];
+// let result = [];
+
+// numbers.forEach((num, i) => {
+//   result.push(num + i);
+// });
+// console.log(result)
+
+// #11
+// let counter = 0;
+// for (let i = 1; i <= 10; i++) {
+//   counter++;
+// }
+// console.log(counter)
+
+// #12
+
+// let number = 9;
+// for (leti = 1; i <= 10; i++) {
+//   let result = number * i + 1;
+//   console.log(result);
+// }
+
+// #13
+
+let result = [];
+
+const reverseFunc = (arr) => {
+  arr.forEach((num) => {
+    result.push(num * -1);
+  });
+};
+
+reverseFunc(numbers);
+
+console.log(result);
+
+// #14
+
+const fizzBuzzFunc = function (number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      result.push("FizzBuzz");
+    } else if (i % 5 == 0) {
+      result.push("Buzz");
+    } else if (i % 3 == 0) {
+      result.push("Fizz");
+    } else {
+      result.push(i);
+    }
+  }
+};
+
+fizzBuzzFunc(25);
+
+console.log(result);
+
+//  #15
+
+const personObj = {
+  name: "Ozodbek",
+  lastName: "Tursunaliyev",
+  age: 17,
+  region: "Fergana",
+  isMerried: false,
+  job: "Programmer",
+  haqida: function () {
+    return;
+    `Assalomu Alaykum, Mening ismim ${this.name}, Familyam ${
+      this.lastName
+    }. Yoshim ${this.age}da. ${this.region}da yashayaman. ${
+      this.job
+    } bo'lib faoliyat olib boraman. ${
+      this.isMerried ? "Uylanganman" : "Hali uylganmaganman"
+    }`;
+  },
+};
